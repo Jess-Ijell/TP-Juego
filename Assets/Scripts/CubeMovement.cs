2fe303ae525bcc762d32d41d96f5f7f579713c26
Jessi Ijelman Lipovetzky <46876160@est.ort.edu.ar>
2022-06-02 14:16:43 -0300

    Initial commit

---
+++ Assets/Scripts/CubeMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+{
+    public float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(movementSpeed, 0, 0);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(movementSpeed * 2, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(0, 0, -movementSpeed * 2);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(0, 0, movementSpeed);
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/RainObjects.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainObjects : MonoBehaviour
+{
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int tiempo = Mathf.FloorToInt(Time.time);
+        int contador = 0;
+
+        while (tiempo > contador && contador <= 10)
+        {
+            Instantiate(prefab);
+            contador += 1;
+        }
+        
+        
+    }
+}
